b92d2d814612a5d1e599ba5f5f0f46160812e835
Jingyi Wang <shizuwang1209@gmail.com>
2017-10-13 21:39:23 -0700

    centralize the register panel

--- src/Components/container/sinUpForm.js
+++ src/Components/container/sinUpForm.js
@@ -1,31 +1,26 @@
-import React, {Component} from 'react';
-import SinUpComp from '../presenter/sinUpComp'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {userSinupRequest} from '../../Actions/authActions'
-import {toast} from 'react-toastify'
+import React, { Component } from "react";
+import SinUpComp from "../presenter/sinUpComp";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { userSinupRequest } from "../../Actions/authActions";
+import { toast } from "react-toastify";
 
-
-class SinUpForm extends Component
-{
-  render()
-  {
-    const {userSinupRequest} = this.props;
-    return(
-  <div className='row'>
-  <div className = "col-md-4 col-md-offset-4">
-    <h2 style={{textAlign: 'center'}}>Register</h2>
-    <SinUpComp userSinupRequest = {userSinupRequest}/>
-    </div>
- </div>
-    )
-
+class SinUpForm extends Component {
+  render() {
+    const { userSinupRequest } = this.props;
+    return (
+      <div className="row" style={{flexDirection:'column'}}>
+        <div className="col-md-4 col-md-offset-4">
+          <h2 style={{ textAlign: "center" }}>Register</h2>
+          <SinUpComp userSinupRequest={userSinupRequest} />
+        </div>
+      </div>
+    );
   }
-
 }
 /*this props used to pass to the presenter, for the action that will be used*/
 SinUpForm.propType = {
   userSinUpRequest: PropTypes.func.isRequired
-}
+};
 
-export default connect(null, {userSinupRequest})(SinUpForm);
+export default connect(null, { userSinupRequest })(SinUpForm);
