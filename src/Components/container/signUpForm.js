216adbb2d5413f59e6ada4d3c67b0860b61485cf
Jingyi Wang <jingyiwang@Jingyis-MacBook-Pro.local>
2017-10-14 15:03:44 -0700

    use redux form and add index.js in Action folder to import and export all action files

--- src/Components/container/signUpForm.js
+++ src/Components/container/signUpForm.js
@@ -1,26 +1,28 @@
 import React, { Component } from "react";
-import SinUpComp from "../presenter/sinUpComp";
+import SignUpComp from "../presenter/signUpComp";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { userSinupRequest } from "../../Actions/authActions";
+import { userSignupRequest } from "../../Actions/authActions";
 import { toast } from "react-toastify";
 
-class SinUpForm extends Component {
+
+
+class SignUpForm extends Component {
   render() {
-    const { userSinupRequest } = this.props;
+    const { userSignupRequest } = this.props;
     return (
       <div className="column" style={{flexDirection:'column', alignItem:'center', justifyContent:'center'}}>
         <div className="col-md-4 col-md-offset-4">
           <h2 style={{ textAlign: "center" }}>Register</h2>
-          <SinUpComp userSinupRequest={userSinupRequest} />
+          <SignUpComp userSignupRequest={userSignupRequest} />
         </div>
       </div>
     );
   }
 }
 /*this props used to pass to the presenter, for the action that will be used*/
-SinUpForm.propType = {
-  userSinUpRequest: PropTypes.func.isRequired
+SignUpForm.propType = {
+  userSignUpRequest: PropTypes.func.isRequired
 };
 
-export default connect(null, { userSinupRequest })(SinUpForm);
+export default connect(null, { userSignupRequest })(SignUpForm);
