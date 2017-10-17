7b6ab1cf179b9d7b008e669eea6e63504719b53c
Jingyi Wang <shizuwang1209@gmail.com>
2017-10-17 15:30:10 -0700

    wrap up Popup panel component and SideButton component

--- src/Components/container/popupSearch.js
+++ src/Components/container/popupSearch.js
@@ -1,71 +1,93 @@
-import React, {Component} from 'react'
-import {Button} from 'framework7-react'
-import Autocomplete from 'react-md/lib/Autocompletes';
-import DatePicker from 'react-md/lib/Pickers/DatePickerContainer';
-import Radio from 'react-md/lib/SelectionControls/Radio';
+import React, { Component } from "react";
+import { Popup, Button } from "framework7-react";
+import Autocomplete from "react-md/lib/Autocompletes";
+import DatePicker from "react-md/lib/Pickers/DatePickerContainer";
+import Radio from "react-md/lib/SelectionControls/Radio";
 
-class PopupSearch extends Component
-{
-  constructor()
-  {
+class PopupSearch extends Component {
+  constructor() {
     super();
-    this.state=
-    {
-      inlineValue: 'A'
-    }
+    this.state = {
+      inlineValue: "A"
+    };
   }
 
-
-  handleInlineChange(e)
-  {
-  this.setState({ inlineValue: e.target.value });
+  handleInlineChange(e) {
+    this.setState({ inlineValue: e.target.value });
   }
 
-  render()
-  {
-    const location= ['北京','河南','山西']
-    return(
-      <div>
-      <Button  color='black' big={true} iconMaterial={'clear' } closePopup={true} style={{width:'10%'}}></Button>
-      <div style={{marginLeft:30, marginTop:30}}>
-    <Autocomplete
-         style={{marginRight:30, width: 300, display:'block', marginBottom:30}}
-         id="location list"
-         label="请输入目的地,城市,国家"
-         className="md-cell md-cell--4"
-         data={location}
-       />
-       <DatePicker
-       style={{width:300}}
-       icon={null}
-        id="appointment"
-          label="选择出发时间"
-            className="md-cell"
-              />
-              <fieldset onChange={this.handleInlineChange.bind(this)} style={{marginTop: 43, marginLeft:-8}}>
+  render() {
+    const location = ["北京", "河南", "山西"];
+    return (
+      <Popup
+        tabletFullscreen
+        style={{ zIndex: 10600 }}
+        opened={this.props.popup}
+      >
+        <div>
+          <Button
+            onClick={this.props.close_popup}
+            color="black"
+            big={true}
+            iconMaterial={"clear"}
+            style={{ width: "10%" }}
+          />
+          <div style={{ marginLeft: 30, marginTop: 30 }}>
+            <Autocomplete
+              style={{
+                marginRight: 30,
+                width: 300,
+                display: "block",
+                marginBottom: 30
+              }}
+              id="location list"
+              label="请输入目的地,城市,国家"
+              className="md-cell md-cell--4"
+              data={location}
+            />
+            <DatePicker
+              style={{ width: 300 }}
+              icon={null}
+              id="appointment"
+              label="选择出发时间"
+              className="md-cell"
+            />
+            <fieldset
+              onChange={this.handleInlineChange.bind(this)}
+              style={{ marginTop: 43, marginLeft: -8 }}
+            >
               <Radio
                 id="inlineRadio1"
                 inline
                 name="inlineRadios"
                 value="A"
                 label="导游活动"
-                checked={this.state.inlineValue === 'A'}
-                       />
-                       <Radio
-                         id="inlineRadio2"
-                         inline
-                         name="inlineRadios"
-                         value="B"
-                         label="游客需求"
-                         checked={this.state.inlineValue === 'B'}
-                       />
-              </fieldset>
-          <Button big={true} raised={true} style={{width: 300, marginTop: 30}} closePopup={true} bg="pink" color="white">搜索</Button>
-       </div>
-      </div>
-    )
+                checked={this.state.inlineValue === "A"}
+              />
+              <Radio
+                id="inlineRadio2"
+                inline
+                name="inlineRadios"
+                value="B"
+                label="游客需求"
+                checked={this.state.inlineValue === "B"}
+              />
+            </fieldset>
+            <Button
+              big={true}
+              raised={true}
+              style={{ width: 300, marginTop: 30 }}
+              closePopup={true}
+              bg="pink"
+              color="white"
+            >
+              搜索
+            </Button>
+          </div>
+        </div>
+      </Popup>
+    );
   }
 }
 
-
-export default PopupSearch
+export default PopupSearch;
---
+++ src/Pages/sideButton.js
@@ -0,0 +1,43 @@
+import React, {Component} from "react";
+import { Fab, Icon } from "framework7-react";
+
+
+class SideButton extends Component {
+  render(){
+
+    return (
+      <div>
+      <Fab
+        color="pink"
+        style={{
+          bottom: 110,
+          width: 40,
+          height: 40,
+          right: 30,
+          backgroundColor: "#3d9fe7"
+        }}
+      >
+        <Icon icon="icon-plus" />
+      </Fab>
+
+      <Fab
+        color="pink"
+        style={{
+          bottom: 60,
+          width: 40,
+          height: 40,
+          right: 30,
+          backgroundColor: "#16c53d"
+        }}
+        onClick={this.props.onBtnClick}
+      >
+        <Icon material="search" />
+      </Fab>
+      </div>
+    )
+
+}
+
+}
+
+export default SideButton;
