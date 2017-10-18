4426277cb62b994cebf131d5ac482bac3c88fa1a
Jingyi Wang <shizuwang1209@gmail.com>
2017-10-18 16:16:00 -0700

    add body of MyAccount

--- src/Pages/MyAccount.js
+++ src/Pages/MyAccount.js
@@ -1,14 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import * as actions from '../Actions';
+import { connect } from 'react-redux';
 
 class MyAccount extends Component {
 
-    render(){
-        return(
-            <div>This is my account</div>
+    onBtnClick(){
+        this.props.logout();
+    }
 
-        )
+    render() {
+        return (
+            <div
+                style={{
+                    display:'flex',
+                    flexDirection:'column',
+                    maxWidth: 800,
+                    margin: "auto",
+                    marginBottom: 98,
+                    marginLeft: 5,
+                    marginRight: 5,
+                    marginTop: 20
+                }}
+            >
+                <h5 style={{ textAlign: "center" }}>我的事项</h5>
+                <div>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            border:'1px solid #e0e0e0'
+                        }}
+                    >
+                        <div style={{ display: "flex", padding:'10px' }}>
+                            <img src="https://placeholdit.co//i/80x80?&text=portrait" />
+                            <span style={{marginLeft:'20px'}}>
+                                <p>用户名称</p>
+                                <p>登录账号</p>
+                            </span>
+                        </div>
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItem: "center",
+                                justifyContent: "center",
+                                paddingRight:'10px'
+                            }}
+                        >
+                            <img src="https://placeholdit.co//i/50x50?&text=QR" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <ul className="collection" style={{marginBottom:'2'}}>
+                        <li className="collection-item">我的朋友圈</li>
+                        <li className="collection-item">我的收藏</li>
+                        <li className="collection-item">我的活动 | 需求</li>
+                        <li className="collection-item">我的账单</li>
+                        <li className="collection-item">关于我们</li>
+                        <li className="collection-item">用户反馈</li>
+                        <li className="collection-item">版本更新</li>
+                    </ul>
+                </div>
+                <button className='red lighten-1 white-text' style={{marginTop:'0'}} onClick={this.onBtnClick.bind(this)}>退出账户</button>
+            </div>
+        );
     }
 }
 
-
-export default MyAccount
+export default connect(null, actions)(MyAccount);
