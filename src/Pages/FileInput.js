80ac38c3b06af36f81ac6d408656f8acc24a7b1c
Jingyi Wang <jingyiwang@Jingyis-MacBook-Pro.local>
2017-10-19 21:36:11 -0700

    write image upload/drag/preview using Dropzone with Reduxform

--- src/Pages/FileInput.js
+++ src/Pages/FileInput.js
@@ -1,5 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import Dropzone from "react-dropzone";
+
+class FileInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { src: "" };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    const { input: { onChange } } = this.props;
+    const reader = new FileReader();
+    reader.onload = () => {
+      // console.log('src', reader.result);
+      this.setState({ src: reader.result });
+    };
+    reader.readAsDataURL(e.target.files[0]);
+    onChange(e.target.files[0]);
+  }
+  render() {
+    const { input: { value }, touched, error } = this.props;
+    console.log(value);
+
+    return (
+      <div>
+        <Dropzone
+          style={{ border: "1px solid #e0e0e0" }}
+          name={this.props.name}
+          multiple={true}
+          onDrop={(imagesToUpload, e) => {
+            const reader = new FileReader();
+            reader.onload = () => {
+              this.setState({ src: reader.result });
+              // console.log('src', src)
+            };
+
+            reader.readAsDataURL(imagesToUpload[0]);
+            this.props.input.onChange(imagesToUpload);
+          }}
+        >
+          <img
+            style={{ height: "100px", width: "100%" }}
+            src={this.state.src}
+          />
+        </Dropzone>
+        {touched && error}
+      </div>
+    );
+  }
+}
+
+export default FileInput;
+
+// using redux form
+
+// import Dropzone from "react-dropzone";
 
 // const adaptFileEventToValue = delegate => e => delegate([...e.target.files]);
 
@@ -17,38 +72,3 @@ import Dropzone from "react-dropzone";
 //   />
 // );
 
-export default ({ input, label, name, meta: { error, touched } }) => {
-  const files = input.value;
-  console.log("files", files);
-
-  return (
-    <div>
-      <Dropzone
-        style={{border:'1px solid #000'}}
-        name={name}
-        multiple={true}
-        onDrop={(imagesToUpload, e) => {
-          const reader = new FileReader();
-          reader.onload = () => {
-            const src = reader.result;
-            // console.log('src', src)
-          };
-
-          reader.readAsDataURL(imagesToUpload[0]);
-          input.onChange(imagesToUpload);
-        }}
-      >
-        <img
-          style={{ height: "100px", width:'100%' }}
-          src="https://placeholdit.co//i/800x100?&text=img"
-        />
-      </Dropzone>
-      {touched && error}
-
-      {files &&
-      Array.isArray(files) && (
-        <ul>{files.map((file, index) => <li key={index}>{file.name}</li>)}</ul>
-      )}
-    </div>
-  );
-};
