8e0138b333bb8c8dd2b522ff676ed9f52ac96320
Jingyi Wang <shizuwang1209@gmail.com>
2017-10-19 19:28:10 -0700

    create AddRoute screen

--- src/Pages/AddRoute.js
+++ src/Pages/AddRoute.js
@@ -1,11 +1,169 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { reduxForm, Field } from "redux-form";
+import AddRouteField from "./AddRouteField";
+import FIELDS from "./routeFormFields";
+import FileInput from "./FileInput";
+import * as actions from "../Actions";
 
+class AddRoute extends Component {
+    onCancel() {}
+    submitForm(values) {
+        const {
+            location,
+            budget,
+            date,
+            number_of_people,
+            event_type,
+            service,
+            event_intro,
+            notes,
+            img1,
+            img2
+        } = values;
+        // console.log('username',username);
+        this.props.addRoute({
+            location,
+            budget,
+            date,
+            number_of_people,
+            event_type,
+            service,
+            event_intro,
+            notes,
+            images: [img1[0].name, img2[0].name]
+        });
+    }
 
-const AddRoute = () => {
-    return (
-    <div>This is AddRoute screen</div>
+    renderFields() {
+        return FIELDS.map(({ label, name }) => (
+            <Field
+                key={name}
+                name={name}
+                type="text"
+                component={AddRouteField}
+                label={label}
+            />
+        ));
+    }
+    render() {
+        return (
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    maxWidth: 800,
+                    margin: "auto",
+                    marginBottom: 98,
+                    marginLeft: 5,
+                    marginRight: 5,
+                    marginTop: 20
+                }}
+            >
+                <div>
+                    <i
+                        className="material-icons medium left"
+                        onClick={() => this.props.history.push("/my")}
+                    >
+                        keyboard_arrow_left
+                    </i>
+                    <h5
+                        style={{
+                            borderBottom: "1px solid #e0e0e0",
+                            paddingBottom: "10px"
+                        }}
+                    >
+                        新的旅程，新的心情
+                    </h5>
+                </div>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        marginBottom: "20px"
+                    }}
+                >
+                    <a
+                        className="btn-flat"
+                        style={{
+                            borderRight: "1px solid #e0e0e0",
+                            paddingRight: "20px"
+                        }}
+                    >
+                        <i className="material-icons left">event_note</i>发布活动
+                    </a>
+                    <a className="btn-flat" style={{ paddingLeft: "20px" }}>
+                        <i className="material-icons left">event_note</i>发布需求
+                    </a>
+                </div>
 
-    )
+                <form
+                    onSubmit={this.props.handleSubmit(
+                        this.submitForm.bind(this)
+                    )}
+                >
+                    {this.renderFields()}
+
+                    <Field
+                        name="notes"
+                        component="textarea"
+                        placeholder="说点什么吧"
+                        style={{ height: "10rem", marginBottom: "20px" }}
+                    />
+                    <div>上传文件</div>
+                    <div style={{ display: "flex", maxWidth: "100%" }}>
+                        <Field
+                            component={FileInput}
+                            name="img1"
+                            style={{ padding: "20px 0 20px 0 ", flex: "1" }}
+                        />
+                        <Field
+                            component={FileInput}
+                            name="img2"
+                            style={{ padding: "20px 0 20px 0 ", flex: "1" }}
+                        />
+                        <Field
+                            component={FileInput}
+                            name="img3"
+                            style={{ padding: "20px 0 20px 0 ", flex: "1" }}
+                        />
+                        <Field
+                            component={FileInput}
+                            name="img4"
+                            style={{ padding: "20px 0 20px 0 ", flex: "1" }}
+                        />
+                    </div>
+                    <div style={{ display: "flex" }}>
+                        <button
+                            className="red accent-4"
+                            style={{ flex: "1" }}
+                            onClick={this.onCancel}
+                        >
+                            取消活动
+                        </button>
+                        <button
+                            className="light-blue accent-4"
+                            style={{ flex: "1" }}
+                            type="submit"
+                        >
+                            创建活动
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
-export default AddRoute;
+const validate = values => {
+    const errors = {};
+
+    return errors;
+};
+
+AddRoute = reduxForm({
+    form: "addRouteForm",
+    validate
+})(AddRoute);
+
+export default (AddRoute = connect(null, actions)(AddRoute));
