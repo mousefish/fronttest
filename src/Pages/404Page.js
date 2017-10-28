fa1eb6a007af493e884c8c421f67675f8f869d68
Jingyi Wang <shizuwang1209@gmail.com>
2017-10-27 22:51:23 -0700

    feat: Design and add 404 page image

--- src/Pages/404Page.js
+++ src/Pages/404Page.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-
+import NotFoundPage from './404.png';
 class PageNotFound extends Component
 {
   render()
   {
     return(
   <div>
-    <p>404 Page Not Found</p>
+    <img src={NotFoundPage}/>
  </div>
     )
 
