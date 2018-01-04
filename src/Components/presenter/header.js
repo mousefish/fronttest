9a3cbd81c44409d125da096e18cdc35766830f05
Jingyi Wang <shizuwang1209@gmail.com>
2018-01-04 12:40:12 -0800

    fix: Centralize the content of header

--- src/Components/presenter/header.js
+++ src/Components/presenter/header.js
@@ -1,31 +1,41 @@
 import React from "react";
 import TLogo from "../../Assets/Images/logo.jpg";
 
-const header = (props) => {
+const header = props => {
   return (
-    <div style={{ textAlign: "center", padding: "10px 0" }}>
-      <img
-        src={TLogo}
+    <div>
+      <div
         style={{
-          width: 80,
-          height: 80
-        }}
-      />
-      <span
-        style={{
-          marginLeft: "6vw",
-          color: "#666666",
-          fontWeight: "bold",
-          fontSize: 40,
-          verticalAlign: -14
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
         }}
       >
-        携U行
-      </span>
+        <img
+          src={TLogo}
+          style={{
+            width: 80,
+            height: 80
+          }}
+        />
+        <span
+          style={{
+            marginLeft: "6vw",
+            color: "#666666",
+            fontWeight: "bold",
+            fontSize: 40,
+            verticalAlign: -14
+          }}
+        >
+          携U行
+        </span>
+      </div>
+
       <h6
         style={{
+          textAlign: "center",
           color: "#666666",
-          paddingTop:10
+          paddingTop: 10
         }}
       >
         {props.description}
