7d143433593a0fd6baba1dfd1653f62ab7229043
Jingyi Wang <shizuwang1209@gmail.com>
2018-02-24 21:59:45 -0800

    Create updating user's basic info feature

--- src/Pages/PrivateBasicInfo.js
+++ src/Pages/PrivateBasicInfo.js
@@ -15,61 +15,141 @@ import KeyboardArrowLeft from "material-ui-icons/KeyboardArrowLeft";
 import PersonProfileDetails from "../Components/container/PersonProfileDetails";
 import PageHeader from "./PageHeader";
 import * as actions from "../Actions";
+import Dialog, {
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle
+} from "material-ui/Dialog";
+
+import TextField from "material-ui/TextField";
+import Translation from "../Data/UserBasicInfoENtoCH";
 
 const styles = theme => ({
-    image: {
-        width: "100%",
-        maxWidth: "100%",
-        height: 224,
-        marginBottom: 40
-    },
-
-    innerWrapper: {
-        textAlign: "center"
-    },
-
     button: {
         margin: theme.spacing.unit,
         width: "95%"
     },
-
-    smallButton: {
-        margin: theme.spacing.unit,
-        width: "55%"
-    },
-
-    root: {
-        margin: theme.spacing.unit,
-        width: "95%",
-        padding: 15
-    },
-    flex: {
-        flex: 1
+    errMsg: {
+        color: "red"
     }
 });
 
 class PrivateBasicInfo extends Component {
+    state = {
+        open: false,
+        key: "",
+        value: "",
+        err: ""
+    };
 
-    renderPage(){
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    handleUpdate(data) {
+         //  Needs to clear the err message to avoid showing the previous one
+        this.setState({
+            err:""
+        })
+        // valueToBeUpdated {userId: 23, username: "jingyi"}
+        this.setState({
+            open: true,
+            key: Translation[data.key],
+            value: data.value
+        });
+    }
+
+    submitUpdates = () => {
+        let inputValue = this.state.value;
+        switch (this.state.key) {
+            case "邮箱":
+                if (
+                    !/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+                        inputValue
+                    )
+                ) {
+                    this.setState({
+                        err: "请输入有效邮箱"
+                    });
+                }
+                break;
+
+            case "密码":
+                if (inputValue.length !== 6) {
+                    this.setState({
+                        err: "密码长度为六位"
+                    });
+                }
+                break;
+
+            default:
+                if (!inputValue) {
+                    this.setState({
+                        err: "值不能为空"
+                    });
+                }
+        }
+    };
+
+    renderPage() {
         let originalUser = localStorage.getItem("user");
         let user;
-        if(originalUser){
-           user=JSON.parse(originalUser);
-           return (
-             <div>
-                 <PageHeader history={this.props.history} title="我的信息" />
-                <PersonProfileDetails profile={user} />
-             </div>
-           )
-        }else{
-            return <div>无权访问</div>
+        if (originalUser) {
+            user = JSON.parse(originalUser);
+            return (
+                <div>
+                    <PageHeader history={this.props.history} title="我的信息" />
+                    <PersonProfileDetails
+                        profile={user}
+                        onClick={data => this.handleUpdate(data)}
+                    />
+                </div>
+            );
+        } else {
+            return <div>无权访问</div>;
         }
     }
 
     render() {
         const { classes } = this.props;
+
         return (
             <div className="wrapper">
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    aria-labelledby="form-dialog-title"
+                >
+                    <DialogTitle id="form-dialog-title">
+                        更改{this.state.key}信息
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText style={{ width: 400 }} />
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label={this.state.key}
+                            type="email"
+                            value={this.state.value}
+                            onChange={e => {
+                                this.setState({ value: e.target.value });
+                            }}
+                            fullWidth
+                        />
+                        <div className={classes.errMsg}>{this.state.err}</div>
+                    </DialogContent>
+
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            稍后再说
+                        </Button>
+                        <Button color="primary" onClick={this.submitUpdates}>
+                            更改
+                        </Button>
+                    </DialogActions>
+                </Dialog>
                 {this.renderPage()}
             </div>
         );
