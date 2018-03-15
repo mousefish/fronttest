12266584b37ad2f5ffef38eacfdc30ecde1f8be7
Jingyi Wang <shizuwang1209@gmail.com>
2018-03-15 13:27:36 -0700

    Restyle ActivityIndex

--- src/Components/container/ActivityIndex.js
+++ src/Components/container/ActivityIndex.js
@@ -2,22 +2,20 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import _ from "lodash";
 import { withStyles } from "material-ui/styles";
+import classNames from "classnames";
 import { Link } from "react-router-dom";
+import Card, { CardHeader, CardMedia, CardContent } from "material-ui/Card";
 import LocationOn from "material-ui-icons/LocationOn";
 import AccessTime from "material-ui-icons/AccessTime";
 import LocalOffer from "material-ui-icons/LocalOffer";
 import Star from "material-ui-icons/Star";
 import StarBorder from "material-ui-icons/StarBorder";
 import StarHalf from "material-ui-icons/StarHalf";
-
 import Person from "material-ui-icons/Person";
-
 import travel from "../../Assets/Images/test.jpg";
-
-import Card, { CardHeader, CardMedia, CardContent } from "material-ui/Card";
-
 import List from "material-ui/List";
 import Slide from "material-ui/transitions/Slide";
+import Stars from "../../Pages/Stars";
 
 function Transition(props) {
   return <Slide direction="up" {...props} />;
@@ -39,12 +37,11 @@ const styles = theme => ({
     verticalAlign: "-2px"
   },
 
-  iconHeart:{
+  iconHeart: {
     width: 12,
     height: 12,
     verticalAlign: "-2px"
   },
-
 
   // themeBar: {
   //   position: "absolute",
@@ -74,36 +71,53 @@ const styles = theme => ({
     backgroundColor: "#03A9F4"
   },
 
+  content: {
+    color: "#757575"
+  },
 
-
-  lineWrapper: {
+  infoWrapper: {
     display: "flex",
     flexFlow: "row nowrap",
     justifyContent: "space-between",
     marginBottom: 10,
-    alignItems:"flex-start",
+    alignItems: "flex-start"
+  },
+
+  fav: {
+    textAlign: "right"
+  },
+  // left:{
+  //   border:"1px solid green"
+  // },
+
+  // right:{
+  //   border:"1px solid red"
+  // },
+
+  item: {
+    fontSize: "1.2rem",
+    marginBottom: 6,
+    // border:"1px solid red"
   }
 });
 
 class ActivityIndex extends Component {
-  renderStar(num) {
-    const { icon } = this.props.classes;
-    const starWrapper = [];
 
-    for (let i = 0; i < 5; i++) {
-      if (num - i > 0 && num - i < 1) {
-        starWrapper[i] = <StarHalf key={i} className={icon} />;
-      } else if (i < num) {
-        starWrapper[i] = <Star key={i} className={icon} />;
-      } else {
-        starWrapper[i] = <StarBorder key={i} className={icon} />;
-      }
+  renderStarZone(score) {
+    const { classes } = this.props;
+    if (score > 0) {
+      return <div className={classes.item}><Stars num={score}/></div>;
     }
-
-    return starWrapper;
   }
 
-
+  renderFavZone(num) {
+    const { classes } = this.props;
+    if (num > 0) {
+      return (
+        <div className={classNames(classes.item, classes.fav)}>{num} 人收藏</div>
+      );
+    }
+  }
 
   renderItems() {
     const classes = this.props.classes;
@@ -116,28 +130,27 @@ class ActivityIndex extends Component {
             <CardMedia className={classes.media} image={travel} title="travel">
               <div className={classes.budgetBox}>¥{item.budget}</div>
             </CardMedia>
-            <CardContent style={{ color: "#757575" }}>
+            <CardContent className={classes.content}>
               <h3 style={{ fontWeight: "bold" }}>{item.theme}</h3>
-              <div className={classes.lineWrapper}>
-                <div>
-                  <LocationOn className={classes.icon} /> {item.location}
+              <div className={classes.infoWrapper}>
+                <div className={classes.left}>
+                  <div className={classes.item}>
+                    <LocationOn className={classes.icon} /> {item.location}
+                  </div>
+                  <div className={classes.item}>
+                    <Person className={classes.icon} />
+                    &nbsp;{item.username}
+                  </div>
+                  <div className={classes.item}>
+                    <AccessTime className={classes.icon} /> {item.departdate} 出发
+                  </div>
                 </div>
-                <div>
-                  {this.renderStar(item.averageScore)} &nbsp;{item.numOfRater}{" "}
-                  人评价
-                </div>
-              </div>
 
-              <div className={classes.lineWrapper}>
-                <div>
-                  <Person className={classes.icon} />
-                  &nbsp;{item.username}
-                </div>
-                <div>
-                  {item.likes} 人收藏
+                <div className={classes.right}>
+                  {this.renderStarZone(item.averageScore)}
+                  {this.renderFavZone(item.likes)}
                 </div>
               </div>
-              <div><AccessTime className={classes.icon} /> {item.departdate} 出发</div>
             </CardContent>
           </Card>
         </Link>
