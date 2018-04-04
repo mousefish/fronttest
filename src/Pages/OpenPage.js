c3a307a7bfe44a0f595e6cfa013ca61d65b630a1
Jingyi Wang <shizuwang1209@gmail.com>
2018-04-04 12:17:29 -0700

    Restyle open page

--- src/Pages/OpenPage.js
+++ src/Pages/OpenPage.js
@@ -1,26 +1,43 @@
 import React, { Component } from "react";
 import { withStyles } from "material-ui/styles";
+import Button from "material-ui/Button";
+import AppBar from "material-ui/AppBar";
+import Tabs, { Tab } from "material-ui/Tabs";
 import backgroundWall from "../Assets/Images/backgroundWall.png";
 
 const styles = {
     container: {
         width: "100%",
         maxWidth: 600,
-        margin: "auto"
+        margin: "auto",
+    },
+
+    loginBtnGroup:{
+        textAlign:"center",
+        padding:10,
+        marginTop:20
+    },
+
+    button: {
+        width: "75%",
+        border:"1px solid #1976D2",
+        color:"#1976D2",
+        marginBottom:15,
+        letterSpacing:2,
+        borderRadius:50,
+        padding:"15px 0"
     },
 
     upper: {
         position: "relative",
-        display:"flex",
-        flexFlow:"column",
-        justifyContent:"center",
-        alignItems:"center",
+        display: "flex",
+        flexFlow: "column",
+        justifyContent: "center",
+        alignItems: "center",
         height: 350,
-        marginBottom: 10,
         background: `url(${backgroundWall})`,
         padding: "10px 10px 10px 10px",
-        textAlign: "center",
-        margin: "auto"
+        textAlign: "center"
     },
     overlay: {
         position: "absolute",
@@ -28,19 +45,33 @@ const styles = {
         bottom: 0,
         left: 0,
         right: 0,
-        backgroundColor: "rgba(0,0,80,0.6)",
-
+        backgroundColor: "#1976D2",
+        opacity:0.6
     },
-    inner:{
-      zIndex:1000,
-      width:"100%",
-      margin:"auto",
+    inner: {
+        zIndex: 1000,
+        width: "100%"
+    },
 
+    title: {
+        color: "#fff",
+        marginBottom: 20,
+        fontWeight: "bold"
+    },
+    intro: {
+        color: "#fff",
+        fontSize: "1.2rem",
+        lineHeight: 1.8
     },
 
     image: {
         width: "50%"
     },
+
+    tab: {
+        letterSpacing: 2,
+        backgroundColor:"#1976D2"
+    }
 
     // logoWrapper: {
     //     textAlign: "center",
@@ -53,20 +84,16 @@ const styles = {
     //     maxWidth: 90,
     //     zIndex:-100
     // },
-
-    title: {
-        color: "#fff",
-        marginBottom: 20,
-         fontWeight:"bold"
-
-    },
-    intro: {
-        color: "#fff",
-        fontSize: "1.2rem",
-        lineHeight: 1.8,
-    }
 };
 class OpenPage extends Component {
+    state = {
+        value: 0
+    };
+
+    handleChange = (event, value) => {
+        this.setState({ value });
+    };
+
     render() {
         const { classes } = this.props;
         return (
@@ -74,11 +101,29 @@ class OpenPage extends Component {
                 <div className={classes.upper}>
                     <div className={classes.overlay} />
                     <div className={classes.inner}>
-                    <h2 className={classes.title}>什么是旅行？</h2>
-                    <p className={classes.intro}>
-                        在携U行的定义里，旅行就是像本地人一样去体验，热爱一个城市。没有走马观花，没有强迫消费，有的只是涤荡灵魂，深入骨髓的文化撞击，和地道真实的生活体验。
-                        携U行的唯一使命就是去创造毕生难忘的深度体验。
-                    </p>
+                        <h2 className={classes.title}>什么是旅行？</h2>
+                        <p className={classes.intro}>
+                            在携U行的定义里，旅行就是像本地人一样去体验，热爱一个城市。没有走马观花，没有强迫消费，有的只是涤荡灵魂，深入骨髓的文化撞击，和地道真实的生活体验。
+                            携U行的唯一使命就是去创造毕生难忘的深度体验。
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <AppBar position="static">
+                        <Tabs
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                            fullWidth
+                            indicatorColor="#fff"
+                            textColor="#fff"
+                        >
+                            <Tab className={classes.tab} label="登陆" />
+                            <Tab className={classes.tab} label="注册" />
+                        </Tabs>
+                    </AppBar>
+                    <div className={classes.loginBtnGroup}>
+                        <Button className={classes.button}>用微信登陆</Button>
+                        <Button className={classes.button}>用百度登陆</Button>
                     </div>
                 </div>
             </div>
